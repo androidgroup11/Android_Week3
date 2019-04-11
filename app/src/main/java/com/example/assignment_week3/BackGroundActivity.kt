361107ae09a9ebdcc8d9c7dc739d983b70161ceb
tranduy1441997 <49270829+tranduy1441997@users.noreply.github.com>
2019-04-11 16:41:43 +0700

    Duy lần 1

---
+++ app/src/main/java/com/example/assignment_week3/BackGroundActivity.kt
@@ -0,0 +1,12 @@
+package com.example.assignment_week3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class BackGroundActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_back_ground)
+    }
+}
---
+++ app/src/main/java/com/example/assignment_week3/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.assignment_week3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val Button = BTN1;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

161507986d814219ff2147916dc41bd1a17f2778
tranduy1441997 <49270829+tranduy1441997@users.noreply.github.com>
2019-04-12 11:42:02 +0700

    lần4

--- app/src/main/java/com/example/assignment_week3/BackGroundActivity.kt
+++ app/src/main/java/com/example/assignment_week3/BackGroundActivity.kt
@@ -1,12 +1,18 @@
 package com.example.assignment_week3
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_back_ground.*
 
 class BackGroundActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_back_ground)
+        BtnSave.setOnClickListener {
+            val intent: Intent =  Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

9f5b770452abd6e104478bc5b3f26acb08c1221b
tranduy1441997 <49270829+tranduy1441997@users.noreply.github.com>
2019-04-13 13:15:23 +0700

    LẦN 5

--- app/src/main/java/com/example/assignment_week3/BackGroundActivity.kt
+++ app/src/main/java/com/example/assignment_week3/BackGroundActivity.kt
@@ -1,18 +1,70 @@
 package com.example.assignment_week3
-
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_back_ground.*
 
 class BackGroundActivity : AppCompatActivity() {
-
+    private var currentBackground :String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_back_ground)
-        BtnSave.setOnClickListener {
-            val intent: Intent =  Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        //currentBackground= intent.getStringExtra(CURRENT_BACKGROUND)
+        currentBackground = getString(R.string.ManC)
+        when (currentBackground) {
+            getString(R.string.ManC) -> Btn.setImageResource(R.drawable.a)
+            getString(R.string.Liv) -> Btn.setImageResource(R.drawable.b)
+            getString(R.string.Che) -> Btn.setImageResource(R.drawable.c)
+            getString(R.string.ManU) -> Btn.setImageResource(R.drawable.d)
+            getString(R.string.Asr) -> Btn.setImageResource(R.drawable.e)
+            getString(R.string.Tot) -> Btn.setImageResource(R.drawable.f)
         }
+        BtnIa.setOnClickListener(imgManCClicked)
+        BtnIb.setOnClickListener(imgLivClicked)
+        BtnIc.setOnClickListener(imgCheClicked)
+        BtnId.setOnClickListener(imgManUClicked)
+        BtnIe.setOnClickListener(imgAsrClicked)
+        BtnIf.setOnClickListener(imgTotClicked)
+
+        //BtnSave.setOnClickListener(btnSaveBackgroundClicked)
+        //BtnBackGround.setOnClickListener(btnCancleBackgroundClicked)
     }
+    private val imgManCClicked = View.OnClickListener {
+        BtnIa.setImageResource(R.drawable.a)
+        currentBackground = getString(R.string.ManC)
+    }
+
+    private val imgLivClicked = View.OnClickListener {
+        BtnIb.setImageResource(R.drawable.b)
+        currentBackground = getString(R.string.Liv)
+    }
+
+    private val imgCheClicked = View.OnClickListener {
+        BtnIc.setImageResource(R.drawable.c)
+        currentBackground = getString(R.string.Che)
+    }
+
+    private val imgManUClicked = View.OnClickListener {
+        BtnId.setImageResource(R.drawable.d)
+        currentBackground = getString(R.string.ManU)
+    }
+
+    private val imgAsrClicked = View.OnClickListener {
+        BtnIe.setImageResource(R.drawable.e)
+        currentBackground = getString(R.string.Asr)
+    }
+
+    private val imgTotClicked = View.OnClickListener {
+        BtnIf.setImageResource(R.drawable.f)
+        currentBackground = getString(R.string.Tot)
+    }
+    /*private val btnSaveBackgroundClicked = View.OnClickListener {
+        val intent = Intent()
+        intent.putExtra(CURRENT_BACKGROUND, currentBackground)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+    //private val btnCancleBackgroundClicked = View.OnClickListener {
+        finish()
+    }*/
 }
--- app/src/main/java/com/example/assignment_week3/MainActivity.kt
+++ app/src/main/java/com/example/assignment_week3/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.assignment_week3
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+
+const val CURRENT_BACKGROUND = "current_background"
+const val CURRENT_TITLE = "current_title"
+const val CURRENT_TITLE_COLOR = "current_title_color"
+
+const val BACKGROUND_REQUEST_CODE = 1000
+const val TITLE_REQUEST_CODE = 1001
 
 class MainActivity : AppCompatActivity() {
+    private var currentBackground :String? = null
+    private var currentTitle :String? = null
+    private var currentTitleColor :Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        BtnBackGround.setOnClickListener {
-            val intent: Intent =  Intent(this, BackGroundActivity::class.java)
-            startActivity(intent)
-        }
-        BtnTitle.setOnClickListener {
-            val intent: Intent =  Intent(this, TitleActivity::class.java)
-            startActivity(intent)
-        }
+
+
     }
 }

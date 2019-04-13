18cc2bee813c97b4961d5ba179ac47ac0109062b
tranduy1441997 <49270829+tranduy1441997@users.noreply.github.com>
2019-04-13 13:46:43 +0700

    lần 5

--- app/src/main/java/com/example/assignment_week3/MainActivity.kt
+++ app/src/main/java/com/example/assignment_week3/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.assignment_week3
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val CURRENT_BACKGROUND = "current_background"
 const val CURRENT_TITLE = "current_title"
@@ -18,7 +20,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        BtnBackGround.setOnClickListener {
+            val intent: Intent =  Intent(this, BackGroundActivity::class.java)
+            startActivity(intent)
+        }
+        BtnTitle.setOnClickListener {
+            val intent: Intent =  Intent(this, TitleActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
